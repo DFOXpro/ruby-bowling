e5f4f4020cbcb1618f7d36d8f84b97f3eb7ca993
dfoxpro <dfoxpro@gmail.com>
2019-02-20 18:17:03 -0500

    implemented output file

--- src/controllers/output_controller.rb
+++ src/controllers/output_controller.rb
@@ -4,9 +4,22 @@ class OutputFileController
 		puts text
 	end
 
-	def self.set_output_file(file_route)
-		puts "inputFileController.set_output_file: #{file_route}"
+	def self.set_output_file(file_route = :NO_FILE_OUTPUT)
+		@@output_file_route = File.absolute_path file_route
+		if File.exist? @@output_file_route
+			Error.print :warning_output_file_found, @@output_file_route
+		else
+			Debug.print "Will output in file #{@@output_file_route}"
+		end
 	end
-	# private
+
+
+	def self.write(data, file_route = nil)
+		file_route ||= @@output_file_route
+		File.open(file_route, 'w') do |f|
+			Debug.print "Writing output in file #{f.path}"
+			f.syswrite(data)
+		end
+	end
 	
 end
